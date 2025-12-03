d673a03bc4bf03619bcd86ca1adb060f5fb636a6
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2025-12-03 17:11:14 +0500

    Немного больше тестов config

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,5 +1,6 @@
 ﻿#include <config.hpp>
 #include <doctest.h>
+#include <array>
 
 using namespace studies_schedule;
 
@@ -8,10 +9,35 @@ TEST_SUITE("config")
 
     TEST_CASE("no args")
     {
-        auto config = parseCommandLineParameters({});
+        auto config { parseCommandLineParameters({}) };
         
         CHECK(!config.version);
         CHECK(!config.help);
+        CHECK(config.attempts > 0);
+    }
+
+    TEST_CASE("version")
+    {
+        auto config { parseCommandLineParameters(std::array{ "--version"sv }) };
+
+        CHECK(config.version);
+        CHECK(!config.help);
+
+        config = parseCommandLineParameters(std::array{ "--versionn"sv, "hfjah"sv, "--version"sv });
+        CHECK(config.version);
+        CHECK(!config.help);
+    }
+
+    TEST_CASE("help")
+    {
+        auto config { parseCommandLineParameters(std::array{ "--help"sv }) };
+
+        CHECK(!config.version);
+        CHECK(config.help);
+
+        config = parseCommandLineParameters(std::array{ "--helpp"sv, "hfjah"sv, "--help"sv });
+        CHECK(!config.version);
+        CHECK(config.help);
     }
 
 }
