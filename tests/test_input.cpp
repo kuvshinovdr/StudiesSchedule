6d810d93547302e4450f28ee34ec7fa1debb1609
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2025-12-06 23:03:54 +0500

    test_input.cpp

---
+++ tests/test_input.cpp
@@ -0,0 +1,13 @@
+﻿/// @file  test_input.cpp
+#include <input.hpp>
+#include <doctest.h>
+
+using namespace studies_schedule;
+
+TEST_SUITE("input")
+{
+    TEST_CASE("setForbiddenFields")
+    {
+        // TODO
+    }
+}
