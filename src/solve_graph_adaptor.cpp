4dbd862cde775777d6303646bddddc9856c47702
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2026-01-14 01:40:21 +0500

    Исправление размещения файлов

--- src/solve_graph_adaptor.cpp
+++ src/solve_graph_adaptor.cpp
@@ -1,56 +1,71 @@
-﻿/// @file  solve_graph_adaptor.cpp
 #include "solve_graph_adaptor.hpp"
 
+#include <algorithm>
+#include <unordered_map>
+
 namespace studies_schedule
+
 {
 
-    auto makeSubjectGraph(std::span<Subject const> subjects)
-        -> AdjacencyList
+auto makeSameColoredGraph(Coloring const& coloring)
+    -> AdjacencyList
+{
+    const std::size_t n = coloring.size();
+    AdjacencyList graph(n);
+
+    std::unordered_map<Color, std::vector<VertexIndex>> byColor;
+
+    for (VertexIndex i = 0; i < n; ++i)
+        byColor[coloring[i]].push_back(i);
+
+    // соединяем вершины одинакового цвета
+    for (auto const& [color, vertices] : byColor)
     {
-        // TODO
-        return {};
+        for (std::size_t i = 0; i < vertices.size(); ++i)
+        {
+            for (std::size_t j = i + 1; j < vertices.size(); ++j)
+            {
+                graph[vertices[i]].push_back(vertices[j]);
+                graph[vertices[j]].push_back(vertices[i]);
+            }
+        }
     }
 
-    auto makeForbiddenColorsForSubjects(Task const& task)
-        -> ForbiddenColors
-    {
-        // TODO
-        return {};
-    }
+    return graph;
+}
 
-    auto makeSameColoredGraph(Coloring const& coloring)
-        -> AdjacencyList
-    {
-        // TODO
-        return {};
-    }
+auto indicesOfImpossibleColors(
+        Coloring const& coloring,
+        Color           maxPossibleColor
+    ) -> IndicesOfImpossibleColors
+{
+    IndicesOfImpossibleColors result;
 
-    auto indicesOfImpossibleColors(
-            Coloring const& coloring,
-            Color           maxPossibleColor
-        ) -> IndicesOfImpossibleColors
+    for (VertexIndex i = 0; i < coloring.size(); ++i)
     {
-        // TODO
-        return {};
+        if (coloring[i] > maxPossibleColor)
+            result.push_back(i);
     }
+    return result;
+}
 
-    void updateForbiddenColorsForSubjectGraph(
-            ForbiddenColors&                 forbiddenColorsToModify,
-            Coloring                  const& subjectTimeSlots,
-            IndicesOfImpossibleColors const& subjectsToMove
-        )
+void updateForbiddenColorsForSubjectGraph(
+        ForbiddenColors&                 forbiddenColorsToModify,
+        Coloring                  const& subjectTimeSlots,
+        IndicesOfImpossibleColors const& subjectsToMove
+    )
+{
+    for (VertexIndex i : subjectsToMove)
     {
-        // TODO
-    }
+        Color color = subjectTimeSlots[i];
+        auto& forbidden = forbiddenColorsToModify[i];
 
-    auto makeAssignmentsFromColorings(
-            Task     const& task,
-            Coloring const& timeSlotsAssignment,
-            Coloring const& roomAssignment
-        ) -> Assignments
-    {
-        // TODO
-        return {};
+        if (std::find(forbidden.begin(), forbidden.end(), color)
+            == forbidden.end())
+        {
+            forbidden.push_back(color);
+        }
     }
+}
 
 }
